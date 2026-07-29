a815bdc7865108ac7c931da36121025c271ec799
Emanuel Gonzalez <eg554848@gmail.coml>
2026-07-29 17:13:55 -0600

    dashboard v1

---
+++ src/components/Preventa.jsx
@@ -0,0 +1,20 @@
+import banner from "../assets/img/banner.jpg"
+
+function Preventa() {
+    return (
+        <>
+        
+            <header class="preventa"/>
+
+				<h2>Búsqueda y selección</h2>
+				<p class="text-center h5">Encontramos talento para tu empresa, en todos los cargos administrativos, profesionales y técnicos.</p>
+				<div>
+					<img src={banner} alt="Rcursos Humanos" />
+				</div>
+
+
+        </>
+    )
+}
+
+export default Preventa;
